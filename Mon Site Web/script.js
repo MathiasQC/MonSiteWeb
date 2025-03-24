da5fd46d619e7fd3614af95b0a527b2fb70ab0a7
MathiasQC <schmat@csrdn.qc.ca>
2025-03-24 13:44:37 -0400

    I just added my web site

---
+++ Mon Site Web/script.js
@@ -0,0 +1,5 @@
+document.getElementById('btnClick').addEventListener('click', function() {
+    let resultat = document.getElementById('resultat');
+    resultat.innerText = 'Merci d\'avoir cliqué!';
+    // Vous pouvez ajouter plus de fonctionnalités ici
+});
